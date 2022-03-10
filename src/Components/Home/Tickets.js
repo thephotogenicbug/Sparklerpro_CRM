739ee6ff3246cdbacafa37c2fea03423b92c5b46
thephotogenicbug <navinkumarachari@gmail.com>
2022-03-10 21:23:29 +0530

    10-03-2022 : Fetch Ticket details using Hooks

--- src/Components/Home/Tickets.js
+++ src/Components/Home/Tickets.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import TopBar from "../Navbar/TopBar";
 import { FiEdit3 } from "react-icons/fi";
 import { ImBin } from "react-icons/im";
 import { AiOutlinePlus } from "react-icons/ai";
+import { Link } from "react-router-dom";
 const Tickets = () => {
+
+   const [ticket, processTicket] = useState([]);
+   const getTicket = () => {
+     const url = "http://localhost:5000/getallticket";
+     fetch(url)
+       .then((response) => response.json())
+       .then((allproduct) => processTicket(allproduct));
+   };
+
+   useEffect(() => {
+     getTicket();
+   }, []);
   return (
     <>
       <TopBar title="Tickets" />
@@ -12,9 +25,11 @@ const Tickets = () => {
           <div className="col-md-4"></div>
           <div className="col-md-4"></div>
           <div className="col-md-4">
-            <button className="create_ticket_btn_home">
-              Create New Ticket <AiOutlinePlus className="icons" />
-            </button>
+            <Link to="/">
+              <button className="create_ticket_btn_home">
+                Create New Ticket <AiOutlinePlus className="icons" />
+              </button>
+            </Link>
           </div>
         </div>
         <div className="row table_content">
@@ -33,76 +48,28 @@ const Tickets = () => {
                 </tr>
               </thead>
               <tbody>
-                <tr className="fs-6 fw-normal">
-                  <td>tic #01</td>
-                  <td>Logo</td>
-                  <td>03/03/2022</td>
-                  <td>10/03/2022</td>
-                  <td>Logo, 2nd, Vector File</td>
-                  <td>#500</td>
-                  <td style={{ color: "#187381" }}>Approval</td>
-                  <td class>
-                    <div
-                      style={{
-                        display: "flex",
-                        justifyContent: "space-evenly",
-                        fontSize: "18px",
-                      }}
-                    >
-                      <FiEdit3 />
-                      <ImBin className="text-danger" />
-                    </div>
-                  </td>
-                </tr>
-                <tr className="fs-6 fw-normal">
-                  <td>tic #02</td>
-                  <td>Logo</td>
-                  <td>03/03/2022</td>
-                  <td>10/03/2022</td>
-                  <td>Logo, 2nd, Vector File</td>
-                  <td>#500</td>
-                  <td style={{ color: "#187381" }}>Approval</td>
-                  <td class>
-                    <div
-                      style={{
-                        display: "flex",
-                        justifyContent: "space-evenly",
-                        fontSize: "18px",
-                      }}
-                    >
-                      <FiEdit3 />
-                      <ImBin className="text-danger" />
-                    </div>
-                  </td>
-                </tr>
-                <tr className="fs-6 fw-normal">
-                  <td>tic #03</td>
-                  <td>Logo</td>
-                  <td>03/03/2022</td>
-                  <td>10/03/2022</td>
-                  <td>Logo, 2nd, Vector File</td>
-                  <td>#500</td>
-                  <td style={{ color: "#187381" }}>Approval</td>
-                  <td class>
-                    <div
-                      style={{
-                        display: "flex",
-                        justifyContent: "space-evenly",
-                        fontSize: "18px",
-                      }}
-                    >
-                      <FiEdit3 />
-                      <ImBin className="text-danger" />
-                    </div>
-                  </td>
-                </tr>
+                {ticket.reverse().map((xproduct, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{xproduct.ticketid}</td>
+                      <td>{xproduct.projectname}</td>
+                      <td>{xproduct.date}</td>
+                      <td>{xproduct.duedate}</td>
+                      <td>{xproduct.notes}</td>
+                      <td>{xproduct.budget}</td>
+                      <td>{xproduct.status}</td>
+                      <td>{xproduct.image}</td>
+                    </tr>
+                  );
+                })}
               </tbody>
             </table>
           </div>
         </div>
       </div>
+      <div></div>
     </>
   );
-}
+};
 
-export default Tickets
+export default Tickets;
