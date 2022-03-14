d0c6ef15cd77d7d5caa5d4f32b0339ee0fd765c7
thephotogenicbug <navinkumarachari@gmail.com>
2022-03-14 21:43:07 +0530

    14-03-2022 : Create LinearStepper using MaterialUI

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Components/Home/Home';
 import SideBar from './Components/Navbar/SideBar';
 import TopBar from './Components/Navbar/TopBar';
 import Tickets from './Components/Home/Tickets';
+import LinaerStepper from './Components/CreateTicket/LinaerStepper';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/tickets" element={<Tickets />} />
+          <Route path="/form" element={<LinaerStepper />} />
         </Routes>
       </SideBar>
     </Router>
